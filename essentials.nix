e18159a89346f6aecd76b49e60f60e27ec0ee459
runekid <bugdayci.onur@aol.com>
2025-08-11 23:31:50 +0200

    commit with initial config

---
+++ essentials.nix
@@ -0,0 +1,32 @@
+# /etc/nixos/packages.nix
+{ config, pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    git
+    neovim
+    flatpak
+    vesktop
+    qbittorrent
+    vlc
+    onlyoffice-desktopeditors
+    thunderbird
+    fastfetch
+    steam-run
+    python3Full
+    obs-studio
+    input-remapper
+    gparted
+    htop
+    prismlauncher
+  ];
+
+  #enable flatpak
+  services.flatpak.enable = true;
+
+  programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+}
